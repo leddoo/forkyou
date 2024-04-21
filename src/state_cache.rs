489a35be5d41343e4f45536b4c12b0c2b03fa114
leddoo <leo@leddoo.net>
2024-04-21 11:51:31 +0200

    improve state cache api

--- src/state_cache.rs
+++ src/state_cache.rs
@@ -1,5 +1,6 @@
 use sti::vec::Vec;
 use sti::sync::spin_lock::SpinLock;
+use core::mem::ManuallyDrop;
 
 
 pub struct StateCache<T: Send> {
@@ -9,17 +10,12 @@ pub struct StateCache<T: Send> {
 impl<T: Send> StateCache<T> {
     #[inline]
     pub fn new() -> Self {
-        Self { buffer: SpinLock::new(Vec::new()) }
+        Self::with_cap(crate::ncpu())
     }
 
     #[inline]
     pub fn with_cap(cap: usize) -> Self {
         Self { buffer: SpinLock::new(Vec::with_cap(cap)) }
-    }
-
-    #[inline]
-    pub fn with_cap_ncpu() -> Self {
-        Self::with_cap(crate::ncpu())
     }
 
 
@@ -31,6 +27,38 @@ impl<T: Send> StateCache<T> {
     #[inline]
     pub fn pop(&self) -> Option<T> {
         self.buffer.lock().pop()
+    }
+
+    #[inline]
+    pub fn get(&self, f: impl FnOnce() -> T) -> Cached<T> {
+        let inner = self.pop().unwrap_or_else(f);
+        Cached { cache: self, inner: ManuallyDrop::new(inner) }
+    }
+}
+
+
+pub struct Cached<'a, T: Send> {
+    cache: &'a StateCache<T>,
+    inner: ManuallyDrop<T>,
+}
+
+impl<'a, T: Send> core::ops::Deref for Cached<'a, T> {
+    type Target = T;
+
+    #[inline(always)]
+    fn deref(&self) -> &Self::Target { &self.inner }
+}
+
+impl<'a, T: Send> core::ops::DerefMut for Cached<'a, T> {
+    #[inline(always)]
+    fn deref_mut(&mut self) -> &mut Self::Target { &mut self.inner }
+}
+
+impl<'a, T: Send> core::ops::Drop for Cached<'a, T> {
+    #[inline]
+    fn drop(&mut self) {
+        let inner = unsafe { ManuallyDrop::take(&mut self.inner) };
+        self.cache.push(inner);
     }
 }
 
